02a703f0a4e94719b081d8ba8171e236c583e042
Francis Jemuel Bergonia <francisyuweh@Franciss-MacBook-Pro.local>
2017-10-11 21:46:36 +0800

    Initial Commit

---
+++ Properties/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Properties
+//
+//  Created by Francis Jemuel Bergonia on 11/10/2017.
+//  Copyright © 2017 Francis Jemuel Bergonia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
